ef03d318ee1d2a2c41ea9b0b4128cbc62dcd69a6
Ekaterina Komratova <ekaterina.komratova@gmail.com>
2021-11-25 14:57:34 +0300

    Added my project

---
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+public class CommentsInfo {
+    private String commentatorsName;
+    private int countComments;
+    private boolean openComments;
+
+}
+
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+public class Post {
+    private int postId;
+    private String name;
+    private String imageUrl;
+    private String text;
+    private int date;
+    private int time;
+    private boolean favouritesInfo;
+    private boolean complaintInfo;
+    private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
+}
